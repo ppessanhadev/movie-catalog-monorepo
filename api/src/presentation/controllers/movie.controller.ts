f2f9678b9fb2e1471af48b159cf7514762e17e0d
Pablo Pessanha <pablo.pessanha@play9.com.br>
2022-11-23 00:30:00 -0300

    refactor(api): removes utils folder; moves decorator to presentation layer

--- api/src/presentation/controllers/movie.controller.ts
+++ api/src/presentation/controllers/movie.controller.ts
@@ -2,7 +2,7 @@ import { ApiTags } from '@nestjs/swagger';
 import { Controller, Inject, ParseIntPipe, Query } from '@nestjs/common';
 import { MovieService } from '@domain/movie/movie.service';
 import { MovieResponse } from '@schemas/movie/MovieResponse';
-import { CustomQueryGet } from '@decorators/CustomQueryGet.decorator';
+import { CustomQueryGet } from '@decorators/custom-query-get.decorator';
 
 @ApiTags('Movies Route')
 @Controller('movies')
